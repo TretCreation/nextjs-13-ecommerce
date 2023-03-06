95c0175ff8eb7fda3c40671fbcbca14ca480fae4
Volodymyr <tret.creation@gmail.com>
2023-03-06 02:17:56 +0200

    upd

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,28 +1,17 @@
-import styles from './SearchBar.module.scss'
+import { Search } from '../svgs'
+import styles from './SearcBar.module.scss'
 
 const SearchBar = (): JSX.Element => {
 	return (
 		<form action='' className='relative mx-auto w-max'>
+			<i className={styles['search-icon']}>
+				<Search />
+			</i>
 			<input
 				type='search'
-				className='peer cursor-pointer relative z-10 h-12 w-full rounded-full border bg-transparent 
-				pl-12 
-				outline-none focus:w-full focus:cursor-text focus:border-red-300 focus:pl-16 focus:pr-4'
+				className={styles['search-outline']}
+				placeholder='Search'
 			/>
-			<svg
-				xmlns='http://www.w3.org/2000/svg'
-				className='absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-gray-500 px-3.5 peer-focus:border-red-300 peer-focus:stroke-red-300'
-				fill='none'
-				viewBox='0 0 24 24'
-				stroke='currentColor'
-				strokeWidth='2'
-			>
-				<path
-					strokeLinecap='round'
-					strokeLinejoin='round'
-					d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-				/>
-			</svg>
 		</form>
 	)
 }
--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
 	experimental: {
 		appDir: false
+	},
+
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: [{ loader: '@svgr/webpack', options: { icon: true } }]
+		})
+		return config
 	}
 }
 
