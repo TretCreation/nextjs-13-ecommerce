7bc93ce2fc61405f68e2154746b60d677c103aa4
Volodymyr <tret.creation@gmail.com>
2023-03-19 17:04:07 +0200

    small update

--- components/screens/product/ProductList.tsx
+++ components/screens/product/ProductList.tsx
@@ -1,49 +1,19 @@
-import { Button, ProductItem, Rating } from '@/components'
+import { ProductItem } from '@/components'
 import { ProductService } from '@/services/ProductService'
-import axios from 'axios'
-import Image from 'next/image'
-import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { IProduct } from '../../../typing'
-import styles from './Product.module.scss'
 
 export default function ProductList() {
 	const [products, setProducts] = useState<IProduct[]>([])
 
-	// //* Services
-	// useEffect(() => {
-	// 	const fetchProducts = async () => {
-	// 		const data = await ProductService.getAll()
-	// 		setProducts(data)
-	// 	}
-	// 	fetchProducts()
-	// }, [])
-
-	//* Axios
 	useEffect(() => {
 		const fetchProducts = async () => {
-			const res = await axios.get(`/api/getProducts`)
-
-			setProducts(res.data)
+			const data = await ProductService.getAll()
+			setProducts(data)
 		}
 		fetchProducts()
 	}, [])
 
-	//* Fetch
-	// useEffect(() => {
-	// 	const getProducts = async () => {
-	// 		const res = await fetch(`/api/getProducts`)
-	// 		console.log(res)
-	// 		if (!res.ok) {
-	// 			console.log(res)
-	// 		}
-	// 		const data = await res.json()
-	// 		setProducts(data)
-	// 	}
-	// 	getProducts()
-	// }, [])
-
-	//! not working
 	return (
 		<div className='flex flex-row flex-wrap justify-between'>
 			{products.length ? (
@@ -55,33 +25,4 @@ export default function ProductList() {
 			)}
 		</div>
 	)
-
-	// return (
-	// 	<div className='container mx-auto my-0'>
-	// 		<h1 className={styles.h1}>BEST SELLERS</h1>
-	// 		<div className='flex flex-row flex-wrap justify-between'>
-	// 			{products.map((product: IProduct) => (
-	// 				<div className={styles.card}>
-	// 					<Link href={`products/${product.id}`}>
-	// 						<Image
-	// 							src={product.img}
-	// 							alt={product.name}
-	// 							width={200}
-	// 							height={0}
-	// 							className='rounded shadow'
-	// 						/>
-	// 					</Link>
-	// 					<div className='flex flex-col items-center justify-center p-5'>
-	// 						<Link href={`products/${product.id}`}>
-	// 							<h2 className='text-lg'>{product.name}</h2>
-	// 						</Link>
-	// 						<p>${product.price}</p>
-	// 						<Rating rating={4} />
-	// 						<Button appearance='primary'>Add to cart</Button>
-	// 					</div>
-	// 				</div>
-	// 			))}
-	// 		</div>
-	// 	</div>
-	// )
 }
--- services/ProductService.ts
+++ services/ProductService.ts
@@ -11,27 +11,3 @@ export const ProductService = {
 		}
 	}
 }
-
-//* Fetch
-// const controller = new AbortController()
-// setTimeout(() => controller.abort(), 2000)
-
-// export const ProductService = {
-// 	async getAll() {
-// 		try {
-// 			const res = await fetch(`/api/getProducts`, {
-// 				method: 'GET',
-// 				signal: controller.signal
-// 			})
-// 			// console.log(res)
-// 			if (!res.ok) {
-// 				throw new Error(`Error! status: ${res.status}`)
-// 			}
-// 			const data = await res.json()
-
-// 			return data
-// 		} catch (err) {
-// 			console.log(err)
-// 		}
-// 	}
-// }
