7423d9136d35840eccb42ea2e7bd809468f7edd5
TretCreation <tret.creation@gmail.com>
2026-09-22 21:25:52 +0300

    fix: handle missing cart product

--- src/pages/api/cart/index.ts
+++ src/pages/api/cart/index.ts
@@ -3,46 +3,51 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@/prisma/client'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	if (req.method === 'GET') {
-		try {
-			const data = await prisma.cart.findMany()
-			return res.status(200).json(data)
-		} catch (error) {
-			return res.status(500).json(error)
-		}
-	}
-	if (req.method === 'POST') {
-		try {
-			const { userId } = req.body
+  if (req.method === 'GET') {
+    try {
+      const data = await prisma.cart.findMany()
+      return res.status(200).json(data)
+    } catch (error) {
+      return res.status(500).json(error)
+    }
+  }
+  if (req.method === 'POST') {
+    try {
+      const { userId } = req.body
 
-			const data = await prisma.cart.findMany({
-				where: { userId },
-				select: {
-					product: true
-				}
-			})
+      const data = await prisma.cart.findMany({
+        where: { userId },
+        select: {
+          product: true
+        }
+      })
 
-			const productsWithCount = await Promise.all(
-				data.map(async item => {
-					const count = await prisma.cart.findMany({
-						where: {
-							userId,
-							productId: item.product.id
-						},
-						select: {
-							count: true
-						}
-					})
-					return {
-						...item.product,
-						count: count[0].count
-					}
-				})
-			)
+      const productsWithCount = await Promise.all(
+        data.map(async item => {
+          if (!item.product) {
+            return null
+          }
 
-			return res.status(200).json(productsWithCount)
-		} catch (error) {
-			return res.status(500).json(error)
-		}
-	}
+          const count = await prisma.cart.findMany({
+            where: {
+              userId,
+              productId: item.product.id
+            },
+            select: {
+              count: true
+            }
+          })
+
+          return {
+            ...item.product,
+            count: count[0].count
+          }
+        })
+      )
+
+      return res.status(200).json(productsWithCount)
+    } catch (error) {
+      return res.status(500).json(error)
+    }
+  }
 }
